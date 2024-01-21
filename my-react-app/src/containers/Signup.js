e2ad77eecb0a145219ee731c199caa5337882b48
Chirag125 <106881540+Chirag125@users.noreply.github.com>
2024-01-22 00:31:39 +0530

    added functionalities

--- my-react-app/src/containers/Signup.js
+++ my-react-app/src/containers/Signup.js
@@ -27,26 +27,7 @@ const Signup = ({ signup, isAuthenticated }) => {
         }
     };
 
-    const continueWithGoogle = async () => {
-        try {
-            const res = await axios.get(`${process.env.REACT_APP_API_URL}/auth/o/google-oauth2/?redirect_uri=${process.env.REACT_APP_API_URL}/google`)
-
-            window.location.replace(res.data.authorization_url);
-        } catch (err) {
-
-        }
-    };
-
-    const continueWithFacebook = async () => {
-        try {
-            const res = await axios.get(`${process.env.REACT_APP_API_URL}/auth/o/facebook/?redirect_uri=${process.env.REACT_APP_API_URL}/facebook`)
-
-            window.location.replace(res.data.authorization_url);
-        } catch (err) {
-
-        }
-    };
-
+    
    
 
     return (
@@ -113,13 +94,7 @@ const Signup = ({ signup, isAuthenticated }) => {
                 </div>
                 <button className='btn btn-primary' type='submit'>Register</button>
             </form>
-            <button className='btn btn-danger mt-3' onClick={continueWithGoogle}>
-                Continue With Google
-            </button>
-            <br />
-            <button className='btn btn-primary mt-3' onClick={continueWithFacebook}>
-                Continue With Facebook
-            </button>
+          
             <p className='mt-3'>
                 Already have an account? <Link to='/login'>Sign In</Link>
             </p>
